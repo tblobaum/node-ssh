8e56dd9d43a927a0dc919f813daec61b145f1bd5
James Halliday <substack@gmail.com>
2011-03-13 18:14:31 -0700

    use pkg-config to set include and lib paths

--- ssh.cc
+++ ssh.cc
@@ -2,13 +2,16 @@
 #include <node_object_wrap.h>
 #include <v8.h>
 
+#include <libssh/libssh.h>
+#include <libssh/server.h>
+
 using namespace v8;
 using namespace node;
 
 class SSH {
 private:
-    static Persistent<FunctionTemplate> ctS;
-    static Persistent<FunctionTemplate> ctC;
+    static Persistent<FunctionTemplate> cts;
+    static Persistent<FunctionTemplate> ctc;
 public:
     static void Initialize(Handle<Object> target) {
         HandleScope scope;
@@ -16,37 +19,46 @@ public:
         Local<FunctionTemplate> s = FunctionTemplate::New(Server);
         Local<FunctionTemplate> c = FunctionTemplate::New(Client);
         
-        ctS = Persistent<FunctionTemplate>::New(s);
-        ctC = Persistent<FunctionTemplate>::New(c);
+        cts = Persistent<FunctionTemplate>::New(s);
+        cts->InstanceTemplate()->SetInternalFieldCount(1);
+        cts->SetClassName(String::NewSymbol("Server"));
         
-        ctS->InstanceTemplate()->SetInternalFieldCount(1);
-        ctC->InstanceTemplate()->SetInternalFieldCount(1);
+        ctc = Persistent<FunctionTemplate>::New(c);
+        ctc->InstanceTemplate()->SetInternalFieldCount(1);
+        ctc->SetClassName(String::NewSymbol("Client"));
         
-        ctS->SetClassName(String::NewSymbol("Server"));
-        ctC->SetClassName(String::NewSymbol("Client"));
-        
-        target->Set(String::NewSymbol("Server"), ctS->GetFunction());
-        target->Set(String::NewSymbol("Client"), ctC->GetFunction());
+        target->Set(String::NewSymbol("Server"), cts->GetFunction());
+        target->Set(String::NewSymbol("Client"), ctc->GetFunction());
     }
     
-    static Handle<Value> Server(const Arguments &args) {
-        HandleScope scope;
-        
-        return args.This();
-    }
-    
-    static Handle<Value> Client(const Arguments &args) {
-        HandleScope scope;
-        
-        return args.This();
-    }
+    static Handle<Value> Server(const Arguments &args);
+    static Handle<Value> Client(const Arguments &args);
     
 protected:
     static Handle<Value> New(const Arguments &args);
 };
 
-Persistent<FunctionTemplate> SSH::ctS;
-Persistent<FunctionTemplate> SSH::ctC;
+Persistent<FunctionTemplate> SSH::cts;
+Persistent<FunctionTemplate> SSH::ctc;
+
+Handle<Value> SSH::Server(const Arguments &args) {
+    HandleScope scope;
+    
+    Local<Object> self = args.This();
+    Local<Number> port = Local<Number>::Cast(args[0]);
+    Local<Number> host = Local<Number>::Cast(args[1]);
+    
+    self->Set(String::NewSymbol("port"), port);
+    self->Set(String::NewSymbol("host"), host);
+    
+    return args.This();
+}
+
+Handle<Value> SSH::Client(const Arguments &args) {
+    HandleScope scope;
+    
+    return args.This();
+}
 
 extern "C" void init(Handle<Object> target) {
     HandleScope scope;
--- wscript
+++ wscript
@@ -1,5 +1,6 @@
 import Options
-from os import unlink, symlink
+import os
+import re
 from os.path import exists 
 
 srcdir = '.'
@@ -11,18 +12,30 @@ def set_options(opt):
 def configure(conf):
     conf.check_tool('compiler_cxx')
     conf.check_tool('node_addon')
-    conf.env.append_value('LINKFLAGS', ['-lssh']);
+    
+    if not conf.find_program('pkg-config') :
+        conf.fatal('pkg-config not found')
+    
+    if os.system('pkg-config --exists libssh') != 0 :
+        conf.fatal('libssh pkg-config package (libssh.pc) not found')
 
 def build(bld):
     obj = bld.new_task_gen('cxx', 'shlib', 'node_addon')
     obj.target = 'ssh'
     obj.source = [ 'ssh.cc' ]
-    obj.cxxflags = ['-D_FILE_OFFSET_BITS=64', '-D_LARGEFILE_SOURCE']
+    obj.cxxflags = [ '-D_FILE_OFFSET_BITS=64', '-D_LARGEFILE_SOURCE' ]
+    
+    obj.cxxflags.append(os.popen(
+        'pkg-config --cflags libssh'
+    ).readline().strip())
+    
+    obj.cxxflags.append(os.popen(
+        'pkg-config --libs libssh'
+    ).readline().strip())
 
 def shutdown():
     if Options.commands['clean']:
-        if exists('ssh.node'): unlink('ssh.node')
+        if exists('ssh.node'): os.unlink('ssh.node')
     else:
         if exists('build/default/ssh.node') and not exists('ssh.node'):
-            symlink('build/default/ssh.node', 'ssh.node')
-
+            os.symlink('build/default/ssh.node', 'ssh.node')
